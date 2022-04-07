62d09d3f56e4a7594e86422a799b2dd07157f845
yusufiibrahim <yusufflorineus@gmail.com>
2022-04-07 16:04:10 +0700

    store response data to state

--- src/App.js
+++ src/App.js
@@ -4,18 +4,34 @@ import "./App.css";
 
 function App() {
   const [emojisdata, setEmojisdata] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     // fetching data
     async function fetchEmojis() {
-      const res = await axios.get(
-        "https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28"
-      );
-      console.log(res);
+      setLoading(true);
+
+      try {
+        const res = await axios.get(
+          "https://run.mocky.io/v3/fe964130-70d0-430f-b839-e55081423c28"
+        );
+        setEmojisdata(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+
+        setError(true);
+        setLoading(false);
+      }
     }
 
     fetchEmojis();
   }, []);
+
+  console.log("loading ?", loading);
+  console.log("error ?", error);
+  console.log("emojisData ?", emojisdata);
 
   return (
     <div className="App">
